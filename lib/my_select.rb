57c35da4b75f3ee8fe8948993e37c0ddc39fc077
wang.andy32@gmail.com <wang.andy32@gmail.com>
2018-08-20 19:52:01 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
-
+  counter=0
+  new = []
+  while counter < collection.length
+    if yield collection[counter]
+      new.push(yield collection[counter])
+    end
+  end
+  new
 end
